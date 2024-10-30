d8a5460089dfd885ff9053cfe8daa7b20776c43f
R237092Gseith <seithmutokoyi@gmail.com>
2024-10-30 15:30:07 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+
+def plot_graph_with_coordinates():
+    my_file = open('C:/Users/USER/PycharmProjects/seith/x_y_coordinates.txt','r')
+
+    x_coordinates = []
+    y_coordinates = []
+    my_file.readline()
+    for line in my_file.readlines():
+       x_coordinates.append(float(line.split(',')[0]))
+       y_coordinates.append(float(line.split(',')[1]))
+
+    plt.scatter(x_coordinates, y_coordinates)
+    plt.xlabel('X Coordinates')
+    plt.ylabel('Y Coordinates')
+    plt.title('Scatter Plot of Coordinates')
+    plt.grid(True)
+    plt.show()
+
+# Specify the path to the file and call the function
+plot_graph_with_coordinates()
+
+
+
+
+
+
+
